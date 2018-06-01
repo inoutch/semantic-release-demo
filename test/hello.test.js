1f6ed3e7cafb9222393f26c88240794937ce9629
Akinori Inoue <akiandaki@gmail.com>
2018-06-01 19:31:48 +0900

    fix(3): 3

--- test/hello.test.js
+++ test/hello.test.js
@@ -2,7 +2,7 @@ const expect = require('chai').expect;
 const helloModule = require('../index');
 
 describe('helloworld', () => {
-  it('return helloworld12', () => {
+  it('return helloworld123', () => {
     expect(helloModule.helloworld()).to.equal("hello world");
   });
   it('return goodnight', () => {
